1f296e076a25e86cf3865e92452f848bbad4c475
Nicolas Quintero <quinterorodrigueznicolas@gmail.com>
2023-03-21 17:36:31 +0000

    Update hero initial

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -71,7 +71,7 @@ export default function Hero() {
       <div className="px-6 mx-auto max-w-7xl lg:px-8">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Hola
+            Maweb: Arquitectos Conocidos
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
             Rechazanos, el 50% de arquitectos desconocidos ya lo hicieron
