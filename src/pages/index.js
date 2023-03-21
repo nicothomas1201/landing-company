17a6ce8ea2c5b2b6528fcd5b1bb7571bcea6315b
Nicolas Quintero <quinterorodrigueznicolas@gmail.com>
2023-03-21 19:12:19 +0000

    Update Contact.js and index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
-import Hero from "@/components/Hero"
-import MoreInfo from "@/components/More-info"
-import CtaSection from "@/components/Cta-section"
-import Contact from "@/components/Contact"
+import Hero from "@/components/Hero";
+import MoreInfo from "@/components/More-info";
+import CtaSection from "@/components/Cta-section";
+import Contact from "@/components/Contact";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Maweb</title>
+      </Head>
       <Hero />
       <MoreInfo />
       <CtaSection />
       <Contact />
-      <Contact />
     </>
-  )
+  );
 }
